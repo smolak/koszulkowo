8c27e2c54e9ebcbd5659fc1b03f7f0af858722ff
Jacek Smolak <jacek.smolak@gmail.com>
2019-02-13 22:00:42 +0100

    Add comma

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = (req, res) => {
     const { query } = parse(req.url, true);
     const { name = 'World' } = query;
 
-    res.end(`Hello ${name}!`);
+    res.end(`Hello, ${name}!`);
 };
