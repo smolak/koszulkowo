46239ab3f7590d766c687ef0e9310f131a602986
Jacek Smolak <jacek.smolak@gmail.com>
2019-02-13 22:15:06 +0100

    Test setting an interval

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { parse } = require('url');
 
+//testing interval
+let someValue = 0;
+
+setInterval(() => {
+    someValue++;
+}, 5000);
+
 module.exports = (req, res) => {
     const { query } = parse(req.url, true);
     const { name = 'World' } = query;
 
-    res.end(`Hello, ${name}!`);
+    res.end(`Hello, ${name}! ${someValue}`);
 };
