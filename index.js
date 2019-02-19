1b34e679089da701b3f0a1a995e20f99c519999b
Jacek Smolak <jacek.smolak@gmail.com>
2019-02-19 21:29:00 +0100

    Test static

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const { parse } = require('url');
 
-fs.writeFileSync('date.txt', Date.now(), 'utf8');
+fs.writeFileSync('./static/date.txt', Date.now(), 'utf8');
 
 module.exports = (req, res) => {
     const { query } = parse(req.url, true);
     const { name = 'World' } = query;
-    const date = fs.readFileSync('date.txt', 'utf8');
+    const date = fs.readFileSync('./static/date.txt', 'utf8');
 
     res.end(`Hello, ${name}! ${date}`);
 };
